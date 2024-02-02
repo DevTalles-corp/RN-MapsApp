d3c4be1c1ed49e566d8bfcec252829e8b5091726
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-02 17:05:58 -0500

    inicio de proyecto

---
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export const MapScreen = () => {
+  return (
+    <View>
+      <Text>MapScreen</Text>
+      
+    </View>
+  )
+}
---
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export const PermissionsScreen = () => {
+  return (
+    <View>
+      <Text>PermissionsScreen</Text>
+      
+    </View>
+  )
+}
