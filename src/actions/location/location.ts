bb47d2350c90ca7803e5fd4997656a27cf942f75
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-03 12:01:07 -0500

    Update dependencies and fix geolocation issue

--- src/actions/location/location.ts
+++ src/actions/location/location.ts
@@ -1,27 +1,22 @@
 import Geolocation from '@react-native-community/geolocation';
-import { Location } from '../../infrastructure/interfaces/location';
+import {Location} from '../../infrastructure/interfaces/location';
 
-
-export const getCurrentLocation = async():Promise<Location> => {
-
-  return new Promise(  (resolve, reject) => {
-    Geolocation.getCurrentPosition( (info) => {
-
-      resolve({
-        latitude: info.coords.latitude,
-        longitude: info.coords.longitude
-      })
-
-      
-    }, (error) => {
-      console.log(`Can't get location`);
-      reject(error);
-    })
-
-
-  })
-
-
-
-
-}
+export const getCurrentLocation = async (): Promise<Location> => {
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      info => {
+        resolve({
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        });
+      },
+      error => {
+        console.log(`Can't get location`);
+        reject(error);
+      },
+      {
+        enableHighAccuracy: true,
+      },
+    );
+  });
+};
--- src/presentation/screens/maps/MapScreen.tsx
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -1,14 +1,10 @@
-import {  StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Map } from '../../components/maps/Map';
-import { getCurrentLocation } from '../../../actions/location/location';
+
 
 export const MapScreen = () => {
 
-  // NO HACER!!!
-  getCurrentLocation().then((location) => {
-    console.log(location);
-  })
-
+ 
 
   return (
     <View style={ styles.container}>
