12be6ba5c73fcbf5ff3b2f610f539e3fdf200ca3
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-03 11:40:48 -0500

    Add geolocation functionality

--- src/presentation/screens/maps/MapScreen.tsx
+++ src/presentation/screens/maps/MapScreen.tsx
@@ -1,12 +1,20 @@
 import {  StyleSheet, View } from 'react-native';
 import { Map } from '../../components/maps/Map';
+import { getCurrentLocation } from '../../../actions/location/location';
 
 export const MapScreen = () => {
+
+  // NO HACER!!!
+  getCurrentLocation().then((location) => {
+    console.log(location);
+  })
+
+
   return (
     <View style={ styles.container}>
 
 
-      
+
       {/* <Map /> */}
 
 
