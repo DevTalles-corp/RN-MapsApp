0edeb5a67485622239d6bb0dd1226731ef02ef5c
Fernando Herrera <fernando.herrera85@gmail.com>
2024-02-03 12:31:07 -0500

    Add watchCurrentLocation function to track user's location

--- src/actions/location/location.ts
+++ src/actions/location/location.ts
@@ -20,3 +20,26 @@ export const getCurrentLocation = async (): Promise<Location> => {
     );
   });
 };
+
+export const watchCurrentLocation = (
+  locationCallback: (location: Location) => void,
+): number => {
+  return Geolocation.watchPosition( info => (
+
+    locationCallback({
+      latitude: info.coords.latitude,
+      longitude: info.coords.longitude,
+    })
+
+
+  ), (error) => {
+    throw new Error(`Can't get watchPosition`);
+  }, {
+    enableHighAccuracy: true,
+  });
+};
+
+
+export const clearWatchLocation = (watchId: number) => {
+  Geolocation.clearWatch(watchId);
+}
